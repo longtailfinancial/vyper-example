ff4703b6f439575ac68bc661ee37cf1fba64ed1b
Shawn Anderson <shawn@longtailfinancial.com>
2023-03-19 11:30:34 -0700

    Experimenting with scripts.

---
+++ scripts/deploy.py
@@ -0,0 +1,6 @@
+
+
+
+account = accounts.test_accounts[0]
+beneficiary = accounts.test_accounts[1]
+account.deploy(project.OpenAuction, beneficiary, 0, 1)
---
+++ scripts/timestamps.py
@@ -0,0 +1,9 @@
+import datetime as dt
+
+now = dt.datetime.now()
+
+dt.datetime.timestamp(now) * 1000
+
+print(now)
+
+now.
