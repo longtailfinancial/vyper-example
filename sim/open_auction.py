870fb5b7c572ce2ff57877576b30d32fa284c6fd
Shawn Anderson <shawn@longtailfinancial.com>
2023-03-24 17:47:54 -0700

    Create parameters module for custom parameters.

--- sim/open_auction.py
+++ sim/open_auction.py
@@ -1,41 +1,6 @@
 import param as pm
 import panel as pn
-from web3 import Web3
-import random
-
-
-class Address(pm.String):
-    """Ethereum Address Parameter"""
-
-    def __init__(self, **params):
-        # Empty string or eth address
-        params['regex'] = '^$|^0x[a-fA-F0-9]{40}$'
-        super(Address, self).__init__(**params)
-
-    # def _validate_value(self, val, allow_None):
-        # super(Address, self)._validate_value(val, allow_None)
-        # if not Web3.isAddress(val):
-            # raise ValueError("Address parameter %r must be a valid ethereum address, not %r." % (self.name, val))
-
-    @staticmethod
-    def random_address():
-        return '0x' + ''.join(random.choice('0123456789abcdef') for n in range(40))
-
-    @staticmethod
-    def zero_address():
-        return '0x' + '0' * 40
-
-
-class Uint256(pm.Integer):
-    """Unsigned 256 bit Integer Parameter"""
-    def __init__(self, default=0, softbounds=None, **params):
-        super(Uint256, self).__init__(default=default, bounds=(0,(1<<256)-1), softbounds=softbounds, **params)
-
-
-class Bool(pm.Boolean):
-    """Unsigned 256 bit Integer Parameter"""
-    def __init__(self, default=False, **params):
-        super(Bool, self).__init__(default=default, **params)
+from parameters import Address, Bool, Uint256
 
 
 class OpenAuction(pm.Parameterized):
@@ -56,7 +21,6 @@ class OpenAuction(pm.Parameterized):
     # Keep track of refunded bids so we can follow the withdraw pattern
     # public(HashMap[address, uint256])
     pendingReturns = pm.Dict(default={}) 
-
 
 
 
---
+++ sim/parameters.py
@@ -0,0 +1,32 @@
+import param as pm
+import random
+
+class Address(pm.String):
+    """Ethereum Address Parameter"""
+
+    def __init__(self, **params):
+        # Empty string or eth address
+        params['regex'] = '^$|^0x[a-fA-F0-9]{40}$'
+        super(Address, self).__init__(**params)
+
+    @staticmethod
+    def random_address():
+        return '0x' + ''.join(random.choice('0123456789abcdef') for n in range(40))
+
+    @staticmethod
+    def zero_address():
+        return '0x' + '0' * 40
+
+
+class Uint256(pm.Integer):
+    """Unsigned 256 bit Integer Parameter"""
+    def __init__(self, default=0, softbounds=None, **params):
+        super(Uint256, self).__init__(default=default, bounds=(0,(1<<256)-1), softbounds=softbounds, **params)
+
+
+class Bool(pm.Boolean):
+    """Unsigned 256 bit Integer Parameter"""
+    def __init__(self, default=False, **params):
+        super(Bool, self).__init__(default=default, **params)
+
+
